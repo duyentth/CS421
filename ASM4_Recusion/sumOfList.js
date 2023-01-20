3886735bf34d2735448f729870e312f1f9c1d7be
duyentth <duyen.tran@miu.edu>
2023-01-20 17:24:09 -0600

    have implemented the sumOfList funtion

--- ASM4_Recusion/sumOfList.js
+++ ASM4_Recusion/sumOfList.js
@@ -11,6 +11,17 @@ function sumHelper(L, p) {
     return p.element() + sumHelper(L, L.after(p));
 }
 
+function sumWithRank(L) {
+    if ( L.isEmpty() ) return null;
+    let rank = 0;
+    return sumWithRankHelper(L,rank);
+}
+
+function sumWithRankHelper(L, rank) {
+    if(L.isLast(L.atRank(rank))) return sum = L.elemAtRank(rank);
+    return L.elemAtRank(rank) + sumWithRankHelper(L, ++ rank );
+}
+//L = 2, 4, 1, 5, 6
 let myList = new listMD.List();
 myList.insertFirst(2);
 myList.insertLast(6);
@@ -19,3 +30,4 @@ myList.insertBefore(myList.insertBefore(myList.last(), 5), 1);
 
 console.log("my list is ", myList.toString());
 console.log("sum of list is ", sum(myList));
+console.log("sum of list with rank is ", sumWithRank(myList));
