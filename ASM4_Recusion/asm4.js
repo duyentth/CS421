89f0dc3e0fce424d42c7559ca5e289fbe701fd23
duyentth <duyen.tran@miu.edu>
2023-01-20 23:23:30 -0600

    added C-2-4 answer

--- ASM4_Recusion/asm4.js
+++ ASM4_Recusion/asm4.js
@@ -48,3 +48,30 @@ console.log("my list is ", myList.toString());
 console.log("sum of list is ", sum(myList));
 console.log("sum of list with rank is ", sumWithRank(myList));
 console.log("max of list is ", findMax(myList) );
+
+
+
+function setOfSubsets(n){
+    var binArr = new Array(n);
+    function gen(k,n) {
+        if (k == n){
+            var subSet = [];
+            for(var j=0;j<binArr.length;j++){
+                if (binArr[j] == 1)
+                    subSet.push(j+1);
+            }
+            console.log(subSet);
+        }
+        else {
+            binArr[k] = 0;
+            gen(k+1, n);
+            binArr[k] = 1;
+            gen(k+1, n);
+        }
+    }
+    gen(0,n);
+}
+
+
+
+setOfSubsets(4);
